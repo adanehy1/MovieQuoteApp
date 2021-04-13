8ddf9c1df9cdad95369ee541c553ed94d5a00467
adanehy1 <arron.danehy@gmail.com>
2021-04-13 11:32:39 -0400

    main menu button

--- app/src/main/java/com/example/moviequotev2/FinishScreen.java
+++ app/src/main/java/com/example/moviequotev2/FinishScreen.java
@@ -2,8 +2,11 @@ package com.example.moviequotev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.FileNotFoundException;
 
 public class FinishScreen extends AppCompatActivity {
 
@@ -11,6 +14,10 @@ public class FinishScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_screen);
+    }
+    public void mainMenu(View view) throws FileNotFoundException {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
     @Override
     public void onBackPressed(){
