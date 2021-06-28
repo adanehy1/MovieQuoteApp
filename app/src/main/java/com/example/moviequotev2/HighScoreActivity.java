86d5d66b3891af9061457bd4cbcefdc0916b0842
Adam Cohen <acohen89@binghamton.edu>
2021-06-28 15:42:46 -0700

    Stats in HS activity working

--- app/src/main/java/com/example/moviequotev2/HighScoreActivity.java
+++ app/src/main/java/com/example/moviequotev2/HighScoreActivity.java
@@ -15,44 +15,44 @@ public class HighScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_high_score);
         Stats stats = new Stats(getApplicationContext());
 
-//        TextView textView1 = (TextView) findViewById(R.id.HS1);
-//        textView1.setText("First High Score");
-//
-//        TextView textView2 = (TextView) findViewById(R.id.HS2);
-//        textView2.setText("Second High Score");
-//
-//        TextView textView3 = (TextView) findViewById(R.id.HS3);
-//        textView3.setText("Third High Score");
-//
-//        TextView textView4 = (TextView) findViewById(R.id.HS4);
-//        textView4.setText("Fourth High Score");
-//
-//        TextView textView5 = (TextView) findViewById(R.id.HS5);
-//        textView5.setText("Fifth High Score");
-//
-//        TextView stat1 = (TextView) findViewById(R.id.stat1);
-//        stat1.setText("Rounds Played");
-//
-//        TextView stat2 = (TextView) findViewById(R.id.stat2);
-//        stat2.setText("Average Points Per Round");
-//
-//        TextView stat3 = (TextView) findViewById(R.id.stat3);
-//        stat3.setText("Total Points Accumulated");
-//
-//        TextView stat4 = (TextView) findViewById(R.id.stat4);
-//        stat4.setText("Correct Guess Average");
-//
-//        TextView insStat1 = (TextView) findViewById(R.id.actualStat1);
-//        insStat1.setText(stats.getRoundsPlayed());
-//
-//        TextView insStat2 = (TextView) findViewById(R.id.actualStat2);
-//        insStat2.setText((int) stats.getAvgPointsPerRound());
-//
-//        TextView insStat3 = (TextView) findViewById(R.id.actualStat3);
-//        insStat3.setText((int) stats.getTotalPoints());
-//
-//        TextView insStat4 = (TextView) findViewById(R.id.actualStat4);
-//        insStat4.setText((int) stats.getCorrectGuessAvg());
+        TextView textView1 = (TextView) findViewById(R.id.HS1);
+        textView1.setText("First High Score");
+
+        TextView textView2 = (TextView) findViewById(R.id.HS2);
+        textView2.setText("Second High Score");
+
+        TextView textView3 = (TextView) findViewById(R.id.HS3);
+        textView3.setText("Third High Score");
+
+        TextView textView4 = (TextView) findViewById(R.id.HS4);
+        textView4.setText("Fourth High Score");
+
+        TextView textView5 = (TextView) findViewById(R.id.HS5);
+        textView5.setText("Fifth High Score");
+
+        TextView stat1 = (TextView) findViewById(R.id.stat1);
+        stat1.setText("Rounds Played");
+
+        TextView stat2 = (TextView) findViewById(R.id.stat2);
+        stat2.setText("Average Points Per Round");
+
+        TextView stat3 = (TextView) findViewById(R.id.stat3);
+        stat3.setText("Total Points Accumulated");
+
+        TextView stat4 = (TextView) findViewById(R.id.stat4);
+        stat4.setText("Correct Guess Average");
+
+        TextView insStat1 = (TextView) findViewById(R.id.actualStat1);
+        insStat1.setText(String.valueOf(stats.getRoundsPlayed()));
+
+        TextView insStat2 = (TextView) findViewById(R.id.actualStat2);
+        insStat2.setText(String.valueOf(stats.getAvgPointsPerRound()));
+
+        TextView insStat3 = (TextView) findViewById(R.id.actualStat3);
+        insStat3.setText(String.valueOf(stats.getTotalPoints()));
+
+        TextView insStat4 = (TextView) findViewById(R.id.actualStat4);
+        insStat4.setText(String.valueOf(stats.getCorrectGuessAvg()));
     }
     @Override
 
